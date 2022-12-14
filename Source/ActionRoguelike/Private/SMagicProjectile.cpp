cc219b652505de76b47d8171cb30d0bee3a5533f
saro93 <72972035+saro93@users.noreply.github.com>
2022-12-14 12:01:22 +0100

    SomeFix

--- Source/ActionRoguelike/Private/SMagicProjectile.cpp
+++ Source/ActionRoguelike/Private/SMagicProjectile.cpp
@@ -29,7 +29,7 @@ void ASMagicProjectile::PostInitializeComponents()
 
 void ASMagicProjectile::OnActorOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
 {
-	
+	UE_LOG(LogTemp, Warning, TEXT("Overlapped"));
 	if (OtherActor && OtherActor != GetInstigator()) {
 
 		USAttributeComponent* AttributeComp = Cast<USAttributeComponent>(OtherActor->GetComponentByClass(USAttributeComponent::StaticClass()));
